676d134346a520f43774c3408c0390599c71b3b4
lapivan <lapunoujohn@gmail.com>
2026-09-11 09:55:26 +0300

    Add specializations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { OfficesPage } from './pages/OfficesPage';
 import { OfficeDetailsPage } from './pages/OfficeDetailsPage';
+import { SpecializationsPage } from './pages/SpecializationsPage';
+import { SpecializationDetailsPage } from './pages/SpecializationDetailsPage'; 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/offices" replace />} />
+      <Route path="/" element={<Navigate to="/specializations" replace />} />
 
       <Route path="/offices" element={<OfficesPage />} />
-
       <Route path="/offices/:id" element={<OfficeDetailsPage />} />
+      <Route path="/specializations" element={<SpecializationsPage />} />
+      <Route path="/specializations/:id" element={<SpecializationDetailsPage />} />
     </Routes>
   );
 }
