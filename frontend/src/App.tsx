cb9a19ee566c958b1f242cf60ed447daf76c6154
lapivan <lapunoujohn@gmail.com>
2026-09-14 10:02:50 +0300

    Fix routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
+import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { OfficesPage } from './pages/OfficesPage';
 import { OfficeDetailsPage } from './pages/OfficeDetailsPage';
 import { SpecializationsPage } from './pages/SpecializationsPage';
-import { SpecializationDetailsPage } from './pages/SpecializationDetailsPage'; 
+import { SpecializationDetailsPage } from './pages/SpecializationDetailsPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/specializations" replace />} />
 
-      <Route path="/offices" element={<OfficesPage />} />
-      <Route path="/offices/:id" element={<OfficeDetailsPage />} />
-      <Route path="/specializations" element={<SpecializationsPage />} />
-      <Route path="/specializations/:id" element={<SpecializationDetailsPage />} />
+      <Route path="/offices">
+        <Route index element={<OfficesPage />} />
+        <Route path=":id" element={<OfficeDetailsPage />} />
+      </Route>
+
+      <Route path="/specializations">
+        <Route index element={<SpecializationsPage />} />
+        <Route path=":id" element={<SpecializationDetailsPage />} />
+      </Route>
     </Routes>
   );
 }
