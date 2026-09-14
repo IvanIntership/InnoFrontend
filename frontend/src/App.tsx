e8076dfa1138f360a2b0ec425908ee7abb8e5c25
lapivan <lapunoujohn@gmail.com>
2026-09-14 10:01:22 +0300

    Fix routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,11 +8,17 @@ import { ServiceCategoryDetailsPage } from './pages/ServiceCategoryDetailsPage';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/categories" replace />} />
-      <Route path="/offices" element={<OfficesPage />} />
-      <Route path="/offices/:id" element={<OfficeDetailsPage />} />
-      <Route path="/categories" element={<ServiceCategoriesPage />} />
-      <Route path="/categories/:id" element={<ServiceCategoryDetailsPage />} />
+      <Route path="/" element={<Navigate to="/specializations" replace />} />
+
+      <Route path="/offices">
+        <Route index element={<OfficesPage />} />
+        <Route path=":id" element={<OfficeDetailsPage />} />
+      </Route>
+
+      <Route path="/categories">
+        <Route index element={<ServiceCategoriesPage />} />
+        <Route path=":id" element={<ServiceCategoryDetailsPage />} />
+      </Route>
     </Routes>
   );
 }
